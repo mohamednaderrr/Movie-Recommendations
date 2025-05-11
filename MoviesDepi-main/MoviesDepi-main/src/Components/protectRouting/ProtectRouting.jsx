f5aa6d6dd8d5211dcfa2b517a0ddf9d693e524fa
Mady <150342122+Madyelshshaht@users.noreply.github.com>
2025-05-11 23:14:48 +0300

    Delete MoviesDepi-main/MoviesDepi-main directory

--- MoviesDepi-main/MoviesDepi-main/src/Components/protectRouting/ProtectRouting.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-
-const ProtectRouting = ({ children }) => {
-    const token = localStorage.getItem("token")
-    return token ? children : <Navigate to="/login" state={{ message: "You should log in first" }}/>;
-}
-
-export default ProtectRouting
